e0a018a31280386d62fc6efd2f53d497f299556e
G00403720@atu.ie <G00403720@atu.ie>
2023-10-10 15:42:54 +0100

    Ex1

--- src/main/java/ie/atu/week3/IoC.java
+++ src/main/java/ie/atu/week3/IoC.java
@@ -1,1 +1,22 @@
+package ie.atu.week3;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class IoC {
+    private UserService us;
+
+    public IoC(){
+        us = new UserService();
+    }
+
+    @GetMapping("newUser1/{name}/{email}")
+    public String getUser(@PathVariable String name, @PathVariable String email){
+        us.registerUser(name, email);
+        return "Without IOC";
+    }
+
+}
